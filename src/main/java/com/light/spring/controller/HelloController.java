6d1ea7521c11f934203072b7a2128057af5d2761
linjing <568549815@qq.com>
2018-12-15 15:02:26 +0800

    没更新什么

--- src/main/java/com/light/spring/controller/HelloController.java
+++ src/main/java/com/light/spring/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.light.spring.controller;
 
+import com.light.spring.Entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,9 +28,15 @@ public class HelloController {
     public String  login(){
         return "login";
     }
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String hello(String username,String password) {
-
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String hello(String username, String password, HttpServletRequest request) {
+        HttpSession session=request.getSession();
+        Assert.notNull(username, "账号不能为空！");
+        Assert.notNull(password, "密码不能为空！");
+        User user=new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        session.setAttribute("user",user);
         return "index";
     }
 
